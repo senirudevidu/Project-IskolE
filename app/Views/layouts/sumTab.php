ff771bd8e4a99aeb01dcd54af5958523aa7d8c78
senirudevidu <senhirusenaweera@gmail.com>
2025-07-07 22:32:52 +0530

    sumTab frunt end

--- app/Views/layouts/sumTab.php
+++ app/Views/layouts/sumTab.php
@@ -2,7 +2,6 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <base href="/projectIskole/">
     <link rel="stylesheet" href="public/css/sumTab.css">
