c22966f689cb00162991b531e1eb67575904055d
senirudevidu <senhirusenaweera@gmail.com>
2025-07-12 23:32:30 +0530

    path issue fixed

--- app/Views/Teacher/teacherDashboard.php
+++ app/Views/Teacher/teacherDashboard.php
@@ -3,10 +3,11 @@
     <meta charset="UTF-8">
     <base href="/projectIskole/">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <base href="/projectIskole/"> -->
-    <link rel="stylesheet" href="../../../public/css/teacherDashboard.css">
-    <script src="../../../public\js\teacherNavbar.js"></script>
-    <title>Document</title>
+
+    <base href="/projectIskole/">
+    
+    <link rel="stylesheet" href="public/css/teacherDashboard.css">
+    <script src="public/js/teacherNavbar.js"></script>   
 </head>
 <body>
     <?php include("../layouts/sumTab.php"); ?>
--- app/Views/layouts/sumTab.php
+++ app/Views/layouts/sumTab.php
@@ -3,7 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../../public/css/sumTab.css">
+
+    <base href="/projectIskole/">
+    
+    <link rel="stylesheet" href="public/css/sumTab.css">
 </head>
 <body>
     <?php include 'header.php'; ?>
