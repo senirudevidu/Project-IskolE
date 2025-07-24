7e7ccd402141d0fadc76e528888a239ca7341f91
senirudevidu <senhirusenaweera@gmail.com>
2025-07-24 13:38:59 +0530

    just a commit

--- app/Views/MP/mpDashboard.php
+++ app/Views/MP/mpDashboard.php
@@ -1,6 +1,6 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,16 +13,17 @@
   <link rel="stylesheet" href="../../../public/css/header.css">
   <title>management Panel</title>
 </head>
-<body class="roboto-regular"
->
+
+<body class="roboto-regular">
   <?php include "../layouts/header.php" ?>
   <?php include "../layouts/sumTab.php" ?>
   <?php include "./mpDashboard.html" ?>
 
   <!-- JavaScript -->
   <!-- <script src="../../../public/js/scripts.js"></script> -->
- <!-- <script type="module" src="../../../public/js/vlidation.js"></script> -->
- <script type="module" src="../../../public/js/mpDashboard.js"></script>
+  <!-- <script type="module" src="../../../public/js/vlidation.js"></script> -->
+  <script type="module" src="../../../public/js/mpDashboard.js"></script>
 
 </body>
+
 </html>
--- public/js/parentnavbar.js
+++ public/js/parentnavbar.js
@@ -1,26 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Select all nav items and content sections
-  const navItems = document.getElementsByClassName("nav-item");
-  // Support both .content-section and .bottem as content panels
-  const contentSections = document.getElementsByClassName("content-section");
-  const bottemSections = document.getElementsByClassName("bottem");
+const li = document.getElementsByTagName("li");
+const tabPanel = document.getElementsByClassName("tab-panel");
+var activeIndex = 0;
 
-  // Combine all possible content panels into one array
-  const panels = [...contentSections, ...bottemSections];
-  let activeIndex = 0;
+function tabChange(index) {
+  // Remove active class from the current active tab
+  li[activeIndex].classList.remove("active-item");
 
-  function tabChange(index) {
-    if (navItems[activeIndex]) navItems[activeIndex].classList.remove("active");
-    if (navItems[index]) navItems[index].classList.add("active");
-    if (panels[activeIndex]) panels[activeIndex].classList.remove("active");
-    if (panels[index]) panels[index].classList.add("active");
-    activeIndex = index;
-  }
+  // Add active class to the new tab
+  li[index].classList.add("active-item");
 
-  // Add click event listeners to nav items
-  for (let i = 0; i < navItems.length; i++) {
-    navItems[i].addEventListener("click", function () {
-      tabChange(i);
-    });
-  }
-});
+  // Hide the current tab panel
+  tabPanel[activeIndex].classList.remove("active-tab");
+
+  // Show the new tab panel
+  tabPanel[index].classList.add("active-tab");
+
+  // Update the active index
+  activeIndex = index;
+}
