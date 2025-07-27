cd72e04e5f89e2d9fb5783e6186efc695d2e57ef
senirudevidu <senhirusenaweera@gmail.com>
2025-07-27 13:39:59 +0530

    Create seperate files for hrader, sumtabs , and body

--- app/Views/Teacher/teacherDashboard.php
+++ app/Views/Teacher/teacherDashboard.php
@@ -1,3 +1,21 @@
-<?php include 'teacherHeader.html'; ?>
-<?php include("sumTab.html"); ?>
-<?php include("teacherBody.html"); ?>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />    
+        <base href="/projectIskole/">
+        <link rel="icon" type="image/x-icon" href="/projectIskole/public/assests/logo.png" />
+        <link rel="stylesheet" href="/projectIskole/public/css/header.css" />
+        <link rel="stylesheet" href="/projectIskole/public/css/sumTab.css">
+        <link rel="stylesheet" href="/projectIskole/public/css/Teacher/teacherDashboard.css">
+
+        <script src="/projectIskole/public/js/logout.js"></script>
+        <script src="/projectIskole/public/js/teacherNavbar.js"></script>
+        <title>Teacher Dashboard</title>
+    </head>
+    
+    <body>
+        <?php include 'teacherHeader.html'; ?>
+        <?php include("sumTab.html"); ?>
+        <?php include("teacherBody.html"); ?>
+    </body>
+</html>
