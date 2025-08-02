0ed282046b81d0e16df3522dc6a3196b070bf2b9
senirudevidu <senhirusenaweera@gmail.com>
2025-08-02 20:35:09 +0530

    final front

--- app/Views/Parent/parentDashboard.php
+++ app/Views/Parent/parentDashboard.php
@@ -23,26 +23,36 @@
     <link rel="stylesheet" href="/projectIskole/public/css/parent/parentDashboard.css">
     <link rel="stylesheet" href="/projectIskole/public/css/styles.css">
 
-
-
-    <link rel="stylesheet" href="/public/css/sumTab.css">
-    <link rel="stylesheet" href="/public/css/styles.css">
-    <link rel="stylesheet" href="/public/css/header.css" />
-    <link rel="stylesheet" href="/public/css/parent/parentDashboard.css">
-
-
-
-    <!-- Stylesheets -->
-
-    <link rel="stylesheet" href="/public/css/parent/parentDashboard.css">
-    <link rel="stylesheet" href="/public/css/styles.css">
-    <link rel="stylesheet" href="/public/css/header.css" />
-    <link rel="stylesheet" href="/public/css/sumTab.css">
+    <!-- JavaScript files
+        <script src="/public/js/logout.js"></script>
+        <script src="/public/js/parentnavbar.js"></script> -->
 
     <!-- JavaScript files -->
-    <script src="/public/js/logout.js"></script>
-    <script src="/public/js/parentnavbar.js"></script>
+    <script src="/projectIskole/public/js/logout.js"></script>
+    <script src="/projectIskole/public/js/parentnavbar.js"></script>
+
     <title>Parent Dashboard</title>
+</head>
+
+
+<link rel="stylesheet" href="/public/css/sumTab.css">
+<link rel="stylesheet" href="/public/css/styles.css">
+<link rel="stylesheet" href="/public/css/header.css" />
+<link rel="stylesheet" href="/public/css/parent/parentDashboard.css">
+
+
+
+<!-- Stylesheets -->
+
+<link rel="stylesheet" href="/public/css/parent/parentDashboard.css">
+<link rel="stylesheet" href="/public/css/styles.css">
+<link rel="stylesheet" href="/public/css/header.css" />
+<link rel="stylesheet" href="/public/css/sumTab.css">
+
+<!-- JavaScript files -->
+<script src="/public/js/logout.js"></script>
+<script src="/public/js/parentnavbar.js"></script>
+<title>Parent Dashboard</title>
 </head>
 
 <body class="roboto-regular">
