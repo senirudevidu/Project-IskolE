adc119423a56e8933b0b564288b87452f1867969
adithaanusra <adithaanusara@Adithas-MacBook-Pro.local>
2025-08-02 14:47:47 +0530

    rechange

--- app/Views/Student/studentDashboard.php
+++ app/Views/Student/studentDashboard.php
@@ -1,13 +1,15 @@
 <html>
-    <head>
+
+<head>
         <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />    
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <base href="/projectIskole/">
 
         <!-- Preconnect to Google Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+                rel="stylesheet">
 
         <!-- Fav icon -->
         <link rel="icon" type="image/x-icon" href="public/assests/logo.png" />
@@ -17,17 +19,18 @@
         <link rel="stylesheet" href="public/css/student/student.css">
         <link rel="stylesheet" href="public/css/styles.css">
         <link rel="stylesheet" href="public/css/sumTab.css">
-        
+
 
         <!-- JavaScript files -->
         <script src="public/js/logout.js"></script>
         <script src="public/js/studentNavbar.js"></script>
         <title>Teacher Dashboard</title>
-    </head>
+</head>
 
-    <body class="roboto-regular">
-        <?php include("studentHeader.html"); ?>
-        <?php include("sumTab.html"); ?>
-        <?php include("student.html"); ?>
-    </body>
+<body class="roboto-regular">
+        <?php include("../../../studentHeader.html"); ?>
+        <?php include("../../../sumTab.html"); ?>
+        <?php include("../../../student.html"); ?>
+</body>
+
 </html>
