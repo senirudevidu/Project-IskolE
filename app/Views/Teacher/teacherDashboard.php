734418d5db5773b0b3439248777d050823ff1129
senirudevidu <senhirusenaweera@gmail.com>
2025-08-02 15:46:04 +0530

    just a commt

--- app/Views/Teacher/teacherDashboard.php
+++ app/Views/Teacher/teacherDashboard.php
@@ -17,11 +17,7 @@
         <link rel="stylesheet" href="/projectIskole/public/css/sumTab.css">
         <link rel="stylesheet" href="/projectIskole/public/css/Teacher/teacherDashboard.css">
         <link rel="stylesheet" href="/projectIskole/public/css/styles.css">
-<<<<<<< HEAD
-        
-=======
 
->>>>>>> bdf78557dce6b2b6b39458251a3feaf7a1a7c5d1
         <!-- JavaScript files -->
         <script src="/projectIskole/public/js/logout.js"></script>
         <script src="/projectIskole/public/js/teacherNavbar.js"></script>
