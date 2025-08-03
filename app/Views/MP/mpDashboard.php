c8b53fb82b839b0a4719c5b3e5455554199b1127
Kalana <kalanajinendra@gmail.com>
2025-08-03 13:10:04 +0530

    just a commit

--- app/Views/MP/mpDashboard.php
+++ app/Views/MP/mpDashboard.php
@@ -4,16 +4,23 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+  <!-- Stylesheets -->
   <link rel="stylesheet" href="../../../public/css/management.css">
   <link rel="stylesheet" href="../../../public/css/sumTab.css">
   <link rel="stylesheet" href="../../../public/css/header.css">
-  <title>Document</title>
+  <title>management Panel</title>
 </head>
-<body>
+<body class="roboto-regular"
+>
   <?php include "../layouts/header.php" ?>
   <?php include "../layouts/sumTab.php" ?>
   <?php include "./mpDashboard.html" ?>
 
+  <!-- JavaScript -->
+  <!-- <script src="../../../public/js/scripts.js"></script> -->
+  <script src="../../../public/js/vlidation.js"></script>
   <script src="../../../public/js/mpDashboard.js"></script>
 </body>
 </html>
