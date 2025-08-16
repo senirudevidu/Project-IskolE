04e7762903bcdc634d8b31c931d32ae5590ab0d0
Thasindu-R <ramsithaskt20@gmail.com>
2025-08-16 22:40:08 +0530

    Just a Commit!!!

--- app/Views/Parent/parentDashboard.php
+++ app/Views/Parent/parentDashboard.php
@@ -2,7 +2,7 @@
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />    
-        <base href="/Project-Iskole/">
+        <base href="/project-Iskole/">
 
         <!-- Preconnect to Google Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -10,14 +10,16 @@
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
         <!-- Fav icon -->
-        <link rel="icon" type="image/x-icon" href="/public/assests/logo.png" />
+        <link rel="icon" type="image/x-icon" href="/public/assets/logo.png" />
 
         <!-- Stylesheets -->
-        <link rel="stylesheet" href="/public/css/header.css">
-        <link rel="stylesheet" href="/public/css/parent/parentDashboard.css">
-        <link rel="stylesheet" href="/public/css/styles.css">
-        <link rel="stylesheet" href="/public/css/sumTab.css">
         
+        <link rel="stylesheet" href="/public/css/sumTab.css">
+        <link rel="stylesheet" href="/public/css/styles.css">
+        <link rel="stylesheet" href="/public/css/header.css"/>
+        <link rel="stylesheet" href="/public/css/parent/parentDashboard.css">
+        
+   
 
         <!-- JavaScript files -->
         <script src="/public/js/logout.js"></script>
@@ -26,7 +28,7 @@
     </head>
 
     <body class="roboto-regular">
-        <?php include("parentHeader.html"); ?>
+        <?php include 'parentHeader.html'; ?>
         <?php include("sumTab.html"); ?>
         <?php include("parentDashboard.html"); ?>
     </body>
