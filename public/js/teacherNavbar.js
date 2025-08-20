1c9402df85b54f48264661aa5a56b635aec23079
Seniru Seneweera <152300004+senirudevidu@users.noreply.github.com>
2025-08-20 22:36:15 +0530

    teacher navbar fixed

---
+++ public/js/teacherNavbar.js
@@ -0,0 +1,23 @@
+const li = document.getElementsByTagName("li");
+const tabPanel = document.getElementsByClassName("tab-panel");
+var activeIndex = 0;
+
+function tabChange(index) {
+    // Remove active class from the current active tab
+    li[activeIndex].classList.remove("active-item");
+    
+    // Add active class to the new tab
+    li[index].classList.add("active-item");
+
+    // Hide the current tab panel
+    tabPanel[activeIndex].classList.remove("active-tab");
+    
+    // Show the new tab panel
+    tabPanel[index].classList.add("active-tab");
+
+    // Update the active index
+    activeIndex = index;
+
+}
+
+
