4ce360de35a4e6db67b72dff5c0e51b3a9e3e907
Seniru Seneweera <152300004+senirudevidu@users.noreply.github.com>
2025-08-20 22:20:22 +0530

    path chaged

--- app/Views/Student/studentDashboard.php
+++ app/Views/Student/studentDashboard.php
@@ -10,18 +10,18 @@
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
         <!-- Fav icon -->
-        <link rel="icon" type="image/x-icon" href="public/assests/logo.png" />
+        <link rel="icon" type="image/x-icon" href="../../../public/assests/logo.png" />
 
         <!-- Stylesheets -->
-        <link rel="stylesheet" href="public/css/header.css">
-        <link rel="stylesheet" href="public/css/student/student.css">
-        <link rel="stylesheet" href="public/css/styles.css">
-        <link rel="stylesheet" href="public/css/sumTab.css">
-        
+        <link rel="stylesheet" href="../../../public/css/header.css">
+        <link rel="stylesheet" href="../../../public/css/student/student.css">
+        <link rel="stylesheet" href="../../../public/css/styles.css">
+        <link rel="stylesheet" href="../../../public/css/sumTab.css">
+
 
         <!-- JavaScript files -->
-        <script src="public/js/logout.js"></script>
-        <script src="public/js/studentNavbar.js"></script>
+        <script src="../../../public/js/logout.js"></script>
+        <script src="../../../public/js/studentNavbar.js"></script>
         <title>Student Dashboard</title>
     </head>
 
