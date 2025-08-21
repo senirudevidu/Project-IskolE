e7f3ff0e2ff4903649142e8b310b8a7e012702b1
Kalana <kalanajinendra@gmail.com>
2025-08-21 09:16:33 +0530

    set logout.js file

--- public/js/logout.js
+++ public/js/logout.js
@@ -3,9 +3,5 @@ function logout() {
   sessionStorage.clear();
 
   // Redirect to the login page
-<<<<<<< HEAD
-  window.location.href = "/projectIskole/app/Views/login/login.html";
-=======
   window.location.href = "../../views/login/login.html";
->>>>>>> 716a3dd (complete responsive header)
 }
