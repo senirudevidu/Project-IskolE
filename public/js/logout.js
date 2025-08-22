05d07b95f31e3b0efc42620276576b5c8056412e
Thasindu-R <ramsithaskt20@gmail.com>
2025-08-23 00:28:42 +0530

    Changes!

--- public/js/logout.js
+++ public/js/logout.js
@@ -3,9 +3,5 @@ function logout() {
   sessionStorage.clear();
 
   // Redirect to the login page
-<<<<<<< HEAD
   window.location.href = "/Project-Iskole/app/Views/login/login.html";
-=======
-  window.location.href = "/projectIskole/app/Views/login/login.html";
->>>>>>> 94d743e3dab8bb84ae93e4c5b41966eae28cbc99
 }
