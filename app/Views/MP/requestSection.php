0171f4c1f7ceba235fe88db9ccef10027e4e57b0
Kalana <kalanajinendra@gmail.com>
2025-08-24 14:19:58 +0530

    divide mpDashbord into sections

---
+++ app/Views/MP/requestSection.php
@@ -0,0 +1,49 @@
+<div class="bottem" id="requests">
+    <div class="box">
+        <div class="container info-box-large">
+            <div class="heading-section">
+                <span class="heading-text">Pending Leave Requests</span>
+                <span class="sub-heding-text">Review and approve leave requests</span>
+            </div>
+            <div class="info-box border-container">
+                <div class="left">
+                    <span class="heading-name">K K Jina</span>
+                    <span class="sub-heading">Teacher - Nov 25 2025</span>
+                    <span class="sub-heading-bolt">Medical Leave</span>
+                    <span class="sub-heading">Submitted on Nov 15 2025</span>
+                </div>
+                <div class="right three-com">
+                    <button class="btn btn-green">Approve</button>
+                    <button class="btn btn-red">Reject</button>
+                    <button class="btn">View</button>
+                </div>
+            </div>
+            <div class="info-box border-container">
+                <div class="left">
+                    <span class="heading-name">K K Jina</span>
+                    <span class="sub-heading">Teacher - Nov 25 2025</span>
+                    <span class="sub-heading-bolt">Medical Leave</span>
+                    <span class="sub-heading">Submitted on Nov 15 2025</span>
+                </div>
+                <div class="right three-com">
+                    <button class="btn btn-green">Approve</button>
+                    <button class="btn btn-red">Reject</button>
+                    <button class="btn">View</button>
+                </div>
+            </div>
+            <div class="info-box border-container">
+                <div class="left">
+                    <span class="heading-name">K K Jina</span>
+                    <span class="sub-heading">Teacher - Nov 25 2025</span>
+                    <span class="sub-heading-bolt">Medical Leave</span>
+                    <span class="sub-heading">Submitted on Nov 15 2025</span>
+                </div>
+                <div class="right three-com">
+                    <button class="btn btn-green">Approve</button>
+                    <button class="btn btn-red">Reject</button>
+                    <button class="btn">View</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
