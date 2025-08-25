6d6cebf159e3116a862f982cfc61edd2e4d95b45
Kalana <kalanajinendra@gmail.com>
2025-08-25 08:58:08 +0530

    add addNewUserFormController php file

--- app/Controllers/addnewUserFormController.php
+++ app/Controllers/addnewUserFormController.php
@@ -1,1 +1,17 @@
 <?php
+
+require_once __DIR__ . '/../Models/mp.php';
+require_once __DIR__ . '/../../config/dbconfig.php';
+echo 'addnewUserFormController connected successfully' . "<br>";
+
+if (isset($_POST['submit'])) {
+    echo 'Form submitted successfully' . "<br>";
+    // Form variables
+    $mpId = $_POST['mpId'];
+    $fName = $_POST['fName'];
+    $lName = $_POST['lName'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $dateOfBirth = $_POST['dateOfBirth'];
+    $gender = $_POST['gender'];
+}
