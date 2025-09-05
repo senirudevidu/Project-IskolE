3139153be3751d5e767a43cf8a9606d0b97340ae
Kalana <kalanajinendra@gmail.com>
2025-09-05 12:13:39 +0530

    add sumtabs

---
+++ app/Views/MP/topSection.php
@@ -0,0 +1,18 @@
+<div class="top">
+    <div class="container info-box-small data-box">
+        <span class="info-box-heading">Pending Requests</span>
+        <span class="info-box-sub-heading heading-red">6</span>
+    </div>
+    <div class="container info-box-small data-box">
+        <span class="info-box-heading">Teachers Attendance</span>
+        <span class="info-box-sub-heading heading-green">86.1%</span>
+    </div>
+    <div class="container info-box-small data-box">
+        <span class="info-box-heading">Students Attendance</span>
+        <span class="info-box-sub-heading heading-blue">70.1%</span>
+    </div>
+    <div class="container info-box-small data-box">
+        <span class="info-box-heading">Total relief periods</span>
+        <span class="info-box-sub-heading heading-red">4</span>
+    </div>
+</div>
