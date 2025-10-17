7aa58a6feb398cb7b71c460a76920b443a043f66
Kalana <kalanajinendra@gmail.com>
2025-10-17 18:54:19 +0530

    delete user

--- app/Controllers/addNewUser/addNewUser.php
+++ app/Controllers/addNewUser/addNewUser.php
@@ -1,9 +1,10 @@
 <?php
-require_once __DIR__ . '/../Models/mp.php';
-require_once __DIR__ . '/../Models/user.php';
-require_once __DIR__ . '/../Models/teacher.php';
-require_once __DIR__ . '/../Models/parent.php';
-require_once __DIR__ . '/../Models/student.php';
+ob_start(); // Start output buffering
+require_once __DIR__ . '/../../Models/mp.php';
+require_once __DIR__ . '/../../Models/user.php';
+require_once __DIR__ . '/../../Models/teacher.php';
+require_once __DIR__ . '/../../Models/parent.php';
+require_once __DIR__ . '/../../Models/student.php';
 require_once __DIR__ . '/../../config/dbconfig.php';
 
 
@@ -11,32 +12,38 @@ $connfig = new Database();
 $conn = $connfig->getConnection();
 
 if (isset($_POST['submitUser'])) {
-    echo 'Form submitted successfully' . "<br>";
 
     switch ($_POST['role']) {
         case 'admin':
+
         case 'teacher':
-            echo 'Adding teacher' . "<br>";
+            // echo 'Adding teacher' . "<br>";
             $teacherModel = new Teacher($conn);
             $teacherModel->addTeacher($_POST);
             break;
         case 'student':
-            echo 'Adding student' . "<br>";
+            // echo 'Adding student' . "<br>";
             $studentModel = new Student($conn);
             $studentModel->addStudent($_POST);
             break;
         case 'parent':
-            echo 'Adding parent' . "<br>";
+            // echo 'Adding parent' . "<br>";
             $parentModel = new ParentRole($conn);
             $parentModel->addParent($_POST);
             break;
         case 'mp':
-            echo 'Adding MP' . "<br>";
+            // echo 'Adding MP' . "<br>";
             $mpModel = new Management($conn);
             $mpModel->addMP($_POST);
             break;
         default:
-            echo "Invalid role specified." . "<br>";
+            exit;
+            ob_end_flush(); // Flush the output buffer
             exit;
     }
+
+
+
+    // header("Location: " . $_SERVER['HTTP_REFERER']);
+    // exit;
 }
