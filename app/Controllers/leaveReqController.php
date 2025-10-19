cf9d89b2fa242c5507f09252bf25330f1751e88a
Seniru Seneweera <152300004+senirudevidu@users.noreply.github.com>
2025-10-19 21:57:02 +0530

    absense reasons request working

--- app/Controllers/leaveReqController.php
+++ app/Controllers/leaveReqController.php
@@ -1,5 +1,10 @@
 <?php
 
+// Start session if not already started
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
 include_once __DIR__ . '/../Models/leaveReqModel.php';
 
 class LeaveReqController
@@ -16,5 +21,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['fromDate']) && isset($_POST['toDate']) && isset($_POST['reason'])) {
         $leaveController = new LeaveReqController();
         $leaveController->submitLeaveRequest($_POST['fromDate'], $_POST['toDate'], $_POST['reason']);
+
+
+        // Redirect back to the requests page after submission
+        header("Location: " . $_SERVER['HTTP_REFERER']);
+        exit();
     }
 }
