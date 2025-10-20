93327146cbb2ed21020eed7e7712275edbfb30d3
Kalana <kalanajinendra@gmail.com>
2025-10-20 13:30:40 +0530

    announcement section css

--- app/Views/Parent/parentChildSelect.php
+++ app/Views/Parent/parentChildSelect.php
@@ -1,4 +1,4 @@
-<div class="child-selector container">
+<div class="child-selector container" style="width: 80%;">
   <label for="child-select">Select Child</label>
   <select id="child-select">
     <option>Seniru Seneweera - Grade 10</option>
